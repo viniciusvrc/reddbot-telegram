1ba3e5b174f7a38783b6c660bc465f17e9a7f08f
Oliver Webb <oliver.r.webb@gmail.com>
2019-08-13 12:04:36 +0200

    V3

--- config.py
+++ config.py
@@ -1,17 +1,14 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-'''
-Created on 01.07.2019
-@author: owebb
-'''
-
 # Telegram tipbot variables
 bot_name = ""
 bot_token = ""
 encoding = "utf-8"
-core = "/home/rdd/reddcoind"
-data_origin = "https://coinmarketcap.com/currencies/reddcoin"
+accounts_wallet = "/home/rdd/reddcoind"
+staking_wallet_address = ""
+staking_wallet_rpc_ip = ""
+staking_wallet_rpc_port = ""
+staking_wallet_rpc_user = ""
+staking_wallet_rpc_pw = ""
+market_data_origin = "https://api.coingecko.com/api/v3/coins/reddcoin?localization=false&tickers=false&community_data=false&developer_data=false"
 reddbot_home = "/home/rdd/reddbot/"
 dev_fund_address = "Ru6sDVdn4MhxXJauQ2GAJP4ozpPpmcDKdc"
 dev_fund_balance_api_url = "https://live.reddcoin.com/api/addr/" + dev_fund_address + "/balance"
@@ -23,3 +20,8 @@ reddcoin_rocket_ani = animation_home + "reddcoin_rocket.mp4"
 qrcode_logo_img = image_home + "rdd_qrcode_logo.png"
 hall_of_fame_max_entries = 10
 admin_list = ["admin1", "admin2", "admin3", "admin4"]
+staking_json_file = "staking_tx.json"
+tippings_json_file = "tippings.json"
+users_json_file = "users.json"
+donors_json_file = "donors.json"
+scheduler_active = True
