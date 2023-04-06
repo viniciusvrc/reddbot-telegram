5b0a484972be554a455b258892273912bcb3a682
vinicius vrc <viniciusvrocha@gmail.com>
2023-04-05 22:14:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo "Instalandor iniciado......"
 apt update && apt upgrade -y
-git clone https://github.com/viniciusvrc/reddbot-telegram
 sudo apt-get install python-dev python3-dev libxml2-dev libxslt1-dev zlib1g-dev libsasl2-dev libldap2-dev build-essential libssl-dev libffi-dev libmysqlclient-dev libjpeg-dev libpq-dev libjpeg8-dev liblcms2-dev libblas-dev libatlas-base-dev
 apt-get install python3
 apt-get install python3-pip
